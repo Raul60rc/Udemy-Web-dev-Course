9d38f9a2a3ed76bae902a27c6c4b06588086ee8c
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-02 20:30:21 +0100

    random number roll dice

---
+++ randomNumberGen.js
@@ -0,0 +1,5 @@
+var n = Math.random(); // use () to run the function
+// simulate dice roll 
+n = n * 6;
+n = Math.floor(n) + 1; // the 6 will not show up but adding 1 will so below 7 any random number
+console.log(n);
