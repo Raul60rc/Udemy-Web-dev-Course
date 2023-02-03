3f0d731d54702ad6bdcab1ef466839b0103f63cc
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-03 19:45:09 +0100

    lovescore with conditionals

---
+++ loveCalcuConditions.js
@@ -0,0 +1,12 @@
+// using if & else - controlling the flow 
+maxScore = 100
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore); // with this +1 can go to 100
+
+if (loveScore === 100){
+    alert("your love is!" + loveScore + "You love each other like Kanye loves Kanye");
+}else{
+    alert("your love is" + loveScore + "4")
+
+}
+console.log(loveScore) 
--- loveCalculator.js
+++ loveCalculator.js
@@ -1,4 +1,6 @@
 maxScore = 100
 var loveScore = Math.random();
 loveScore = Math.floor(maxScore); // with this +1 can go to 100
-console.log(loveScore)
+console.log(loveScore) 
+
+// need to fix this 
