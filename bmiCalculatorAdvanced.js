4fa30328e5a183728e04cca14f87d021d418352e
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-08 15:27:25 +0100

    BMI advanced calculator IF/ELSE need to detect error

--- bmiCalculatorAdvanced.js
+++ bmiCalculatorAdvanced.js
@@ -1,12 +1,15 @@
 function bmiCalculator (weight, height) {
-    return interpretation;
+    var bmi = weight / (height * height)
+    return Math.round(bmi);
 }
-if(interpretation < 18.5){
-    return("Your BMI is" + BMI + ", so you are underweight")
+bmiCalculator(81,1.8);
+
+if(bmi < 18.5){
+    return("Your bmi is" + bmi + ", so you are underweight")
 }
-if(interpretation >18.5 && interpretation <= 24.9){
-    return("Your BMI is" + BMI + ", so you have a normal weight")
+if(bmi >18.5 && bmi <= 24.9){
+    return("Your BMI is" + bmi + ", so you have a normal weight")
 }
-if(interpretation >24.9){
-    return("Your BMI is" + BMI + ", so you are overweight")
+if(bmi >24.9){
+    return("Your BMI is" + bmi + ", so you are overweight")
 }
