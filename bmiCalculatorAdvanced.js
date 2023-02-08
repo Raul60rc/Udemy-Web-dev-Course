b0cf3c5d446ed6e7d19f0364d31acfd0c0f38f42
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-08 15:17:50 +0100

    BMI advanced calculator if/else challenge completed

---
+++ bmiCalculatorAdvanced.js
@@ -0,0 +1,12 @@
+function bmiCalculator (weight, height) {
+    return interpretation;
+}
+if(interpretation < 18.5){
+    return("Your BMI is" + BMI + ", so you are underweight")
+}
+if(interpretation >18.5 && interpretation <= 24.9){
+    return("Your BMI is" + BMI + ", so you have a normal weight")
+}
+if(interpretation >24.9){
+    return("Your BMI is" + BMI + ", so you are overweight")
+}
--- loveCalcuConditions.js
+++ loveCalcuConditions.js
@@ -3,9 +3,19 @@ maxScore = 100
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore); // with this +1 can go to 100
 
-if (loveScore === 100){
+if (loveScore > 100){
     alert("your love is!" + loveScore + "You love each other like Kanye loves Kanye");
-}else{
+}
+if (loveScore >30 && loveScore <= 70){
+    alert("your love is" + loveScore)
+}
+if (loveScore <30){
+    alert("your love score is" + loveScore + "not compatible")
+}
+if (loveScore >70 && loveScore <99){
+    alert("your love score is" + loveScore + "great match")
+}
+else{
     alert("your love is" + loveScore + "4")
 
 }
