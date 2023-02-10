c061f31d8ed88242a66d26db73f3a4188cddc20d
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-10 14:11:04 +0100

    Humanage + Dogage calculator

--- bmiCalculatorAdvanced.js
+++ bmiCalculatorAdvanced.js
@@ -1,15 +1,15 @@
 function bmiCalculator (weight, height) {
-    var bmi = weight / (height * height)
+    var bmi = weight / (height * height);
     return Math.round(bmi);
 }
 bmiCalculator(81,1.8);
 
 if(bmi < 18.5){
-    return("Your bmi is" + bmi + ", so you are underweight")
+    return("Your bmi is" + bmi + ", so you are underweight");
 }
 if(bmi >18.5 && bmi <= 24.9){
-    return("Your BMI is" + bmi + ", so you have a normal weight")
+    return("Your BMI is" + bmi + ", so you have a normal weight");
 }
 if(bmi >24.9){
-    return("Your BMI is" + bmi + ", so you are overweight")
+    return("Your BMI is" + bmi + ", so you are overweight");
 }
